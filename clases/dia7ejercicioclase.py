6783de886d1d4438344af3893cf2f16685f8c518
leidy <leidy.recalde995gmail.com>
2019-07-17 16:08:46 -0300

    dia 7 varias cosas

---
+++ clases/dia7ejercicioclase.py
@@ -0,0 +1,19 @@
+#hacer una clase que se llame vehiculo y que tenga tres atributos y uno de ellos que 
+#sea cantidad_rueda, y un metodo que sea definir_vehiculo que me diga si es "bicicleta, auto,
+#  triciclo" de acuerdo de la cantidd de ruedas. 
+#hacer clase para crear objeto
+#class Movil: #definimos la clase es la plantilla(como una reseta para crear algo)
+#    ruedas = 0 #es un atributo de clase o propiedad del obeto que vamos a crear
+#    def __init__ (self, cantidad_rueda): #__init__ es el metodo constructor
+                                    #metodos: son funciones dentro de una clase
+#       self.mi_vehiculo = cantidad_rueda #usamos self para referirnos al objeto mismo
+
+#       print("el vehiculo es", self.mi_vehiculo)# no es obligatorio es para que nos de un mensae de que se cre0
+
+#ruedas= Movil("no es nada")
+class vehiculo:
+    cantida_ruedas = 0
+    color = ""
+    marca = ""
+    def __init__(self, ruedas, )# cada vez que pide metodo tenemos que hacer una funcion
+    
